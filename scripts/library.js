771434ab85ae8b5b44c63b29b65da282a571b037
Allan R <careitan2000@yahoo.com>
2015-06-07 16:42:12 -0700

    Javascript Library functions

--- scripts/library.js
+++ scripts/library.js
@@ -21,6 +21,92 @@ function RegExPhoneChar(TstVar) {
 // END UTILITY AND HELPER FUNCTIONS
 
 // METHOD FUNCTIONS FOR USE ON A WEB FORM
+function ajaxRequest(URL, Type, Parameters) {
+// Populating a null RetVal object.
+// https://piazza.com/class/i0j5uszbfur1jw?cid=238
+// Student Johnathan Moore
+var RetVal = {};
+var blnSuccess;
+var intCode = 0;
+var codeDetailString = '';
+var responseString = '';
+var Param;
+var URLParams = [];
+var req = new XMLHttpRequest();
+if (!req) {
+  throw 'Unable to create HTTPRequest';
+}
+
+// Set Default value of Success Flag.
+blnSuccess = true;
+
+if (Type !== 'GET' && Type !== 'POST') {
+  RetVal.success = false;
+  RetVal.code = 1;
+  RetVal.codeDetail = 'Syntax Error - Populating Type [GET | POST]';
+
+} else if (Parameters === null) {
+  RetVal.success = false;
+  RetVal.code = 3;
+  RetVal.codeDetail = 'Parameters are not populated.';
+} else {
+// Setup the URLParams based off of type of Form.
+for (var Key in Parameters) {
+  if (Parameters.hasOwnProperty(Key)) {
+    if (typeof (Parameters[Key]) !== 'string' && blnSuccess) {
+      RetVal.success = false;
+      RetVal.code = 5;
+      RetVal.codeDetail = 'Parameter[' + i + '] is not a string.';
+
+    } else {
+      Param = encodeURIComponent(Key) + '=' +
+      encodeURIComponent(Parameters[Key]);
+      URLParams.push(Param);
+    }
+  }
+}
+
+// Prepare the WebRequest based on the type of method we are using
+if (Type === 'GET') {
+  req.open('GET', URL + '?' + URLParams.join('&'), true);
+  req.send(null);
+} else {
+// Setting up for POST
+req.open('POST', URL, true);
+req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+req.send(URLParams.join('&'));
+}
+}
+
+// Setup the Async or end state to catch the output
+if (blnSuccess) {
+  req.onreadystatechange = function() {
+    if (this.readyState === 4) {
+      if (this.status !== 200) {
+        RetVal.success = false;
+      } else {
+        RetVal.success = true;
+      }
+
+      RetVal.code = this.status;
+      RetVal.codeDetail = this.responseText;
+      RetVal.response = this.response;
+    }
+  };
+
+  if (!RetVal.code) {
+    RetVal.code = req.status;
+    RetVal.codeDetail = req.responseText;
+    RetVal.response = req.response;
+  }
+
+  return RetVal;
+} else {
+  return RetVal;
+}
+}
+
+// FUNCTIONS CALLED FROM WEB APP OR FORMS
 function Login(){
   var UID = document.getElementById('uid');
   var PWD = document.getElementById('pwd');
